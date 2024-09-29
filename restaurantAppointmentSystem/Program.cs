d95c5a9ec3ebb5987f5c85d0d0b79d78f564ce9d
angelica00101 <angelica_balubayan@dmc.edu.ph>
2024-09-29 12:57:00 +0700

    initial commit

---
+++ restaurantAppointmentSystem/Program.cs
@@ -0,0 +1,11 @@
+﻿//Restaurant Customer Appointment System
+//Functions: Add, remove/edit, view detais
+//customer details components: name, date and time, table number
+//functions: create methods that will add appointments, edit appointments, cancel appointments using linked list
+//functions: create methods using sorted list that will view details of each customers, with exception handling that
+//will not add a detail if it is already existing or already assigned to a customer.
+ class Program
+ {
+
+    
+ }
